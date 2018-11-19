2ef072894375e96f4854ad147d10723d4ee54314
Lukas Heinen <lukas.heinen@bmwiti.onmicrosoft.com>
2018-11-19 21:25:14 +0100

    Add Day15

---
+++ Day15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day15
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var discs = new [,] {
+                {17,1},
+                {7,0},
+                {19,2},
+                {5,0},
+                {3,0},
+                {13,5},
+                {11,0}
+            };
+            
+            
+            var notFound = true;
+            var count = -1;
+            while (notFound)
+            {
+                count++;
+                var allValid = true;
+                for (var i = 0; i < discs.Length/discs.Rank; i++)
+                {
+                    var mod = (discs[i,1] + i + count + 1) % discs[i,0];
+                    if (mod != 0)
+                    {
+                        allValid = false;
+                        break;
+                    }
+                }
+                
+                if (allValid) notFound = false;
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+    }
+}
